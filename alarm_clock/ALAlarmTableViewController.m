a1ee32bc555c9bfb0931f7024b1f9a9bbb61517a
mcarthurgill <mickgill2008@gmail.com>
2014-02-27 14:59:08 -0600

    editing mode

--- alarm_clock/ALAlarmTableViewController.m
+++ alarm_clock/ALAlarmTableViewController.m
@@ -101,12 +101,77 @@
 }
 
 -(void) viewController:(ALNewAlarmViewController *)controller didFinishSettingAlarm:(NSString *)time {
-    [alarmList addObject:time];
+    if (![alarmList containsObject:time]) {
+        [alarmList addObject:time];
+        [setAlarms addObject:time];
+    }else {
+        [setAlarms addObject:time];
+    }
+
     [self.tableView reloadData];
 }
 
 
 - (IBAction)editAlarmAction:(id)sender {
+    if (self.tableView.editing) {
+        [self.tableView setEditing:NO animated:YES];
+    }else {
+        [self.tableView setEditing:YES animated:YES]; 
+    }
 }
+
+
+// // Override to support conditional editing of the table view.
+// - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+// {
+//     return YES;
+// }
+//
+
+
+// // Override to support editing the table view.
+// - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+//{
+//     if (editingStyle == UITableViewCellEditingStyleDelete) {
+//         // Delete the row from the data source
+//         [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+//         
+//         if ([setAlarms containsObject:<#(id)#>]) {
+//             <#statements#>
+//         }
+//     }
+//     else if (editingStyle == UITableViewCellEditingStyleInsert) {
+//     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+//     }
+// }
+
+
+/*
+ // Override to support rearranging the table view.
+ - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+ {
+ }
+ */
+
+/*
+ // Override to support conditional rearranging of the table view.
+ - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+ {
+ // Return NO if you do not want the item to be re-orderable.
+ return YES;
+ }
+ */
+
+/*
+ #pragma mark - Navigation
+ 
+ // In a story board-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+ {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ 
+ */
 
 @end
